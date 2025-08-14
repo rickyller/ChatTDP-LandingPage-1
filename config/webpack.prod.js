096d24dc06b8957636aa5151a9ade81a3ae8140a
Ricardo Murillo <ricardomurillo.udg@gmail.com>
2025-08-13 20:17:09 -0600

    build: fix Node18 OpenSSL hash + isolate bundles to /assets/bundles

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,42 +1,31 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* webpack.prod.js */
 const { merge: Merge } = require('webpack-merge');
 const CommonConfig = require('./webpack.common.js');
 const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = Merge(CommonConfig, {
   output: {
-    filename: '[name]-[hash].bundle.js',
-    path: path.resolve('assets'),
-    publicPath: '/assets/',
-    clean: false // aseguramos que Webpack no limpie automáticamente
+    filename: '[name]-[contenthash].bundle.js',
+    path: path.resolve('assets/bundles'),   // aislamos artefactos
+    publicPath: '/assets/bundles/',         // ruta pública de los bundles
+    hashFunction: 'xxhash64',               // 👈 evita ERR_OSSL_EVP_UNSUPPORTED en Node 18
+    clean: false
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        keep_fnames: true
-      }
-    })]
+    minimizer: [new TerserPlugin({ terserOptions: { keep_fnames: true } })]
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        '**/*',
-        '!images/**',
-        '!images',
-        '!docs/**',
-        '!docs'
-      ],
+      // limpia SOLO dentro de assets/bundles
+      cleanOnceBeforeBuildPatterns: ['**/*'],
       verbose: true
     }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-    }),
+    new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
   ],
 });
